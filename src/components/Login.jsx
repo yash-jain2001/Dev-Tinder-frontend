45aa463645e7c03e844ccdd31ba41ba9a56ba957
Priyanshu jain <89932280+yash-jain2001@users.noreply.github.com>
2026-04-22 21:20:16 +0530

    Login page + handle login api

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState("viratkohli@gmail.com");
+  const [password, setPassword] = useState("Virat@123");
 
-export default Login
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post("http://localhost:3000/login", {
+        email,
+        password,
+      },{
+        withCredentials:true
+      });
+      console.log(res);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center h-full translate-y-[40%]">
+      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+        <legend className="fieldset-legend font-bold text-xl">Login</legend>
+
+        <label className="label text-base">Email</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="input"
+          placeholder="Email"
+        />
+
+        <label className="label text-base">Password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="input"
+          placeholder="Password"
+        />
+
+        <button onClick={handleLogin} className="btn btn-neutral mt-4">
+          Login
+        </button>
+      </fieldset>
+    </div>
+  );
+};
+
+export default Login;
